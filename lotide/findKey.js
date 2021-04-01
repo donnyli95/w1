2cea0f29fc6f211adb6c6e8ee51aca35ff3f56ce
donnyli95 <sz3li@uwaterloo.ca>
2021-04-01 21:55:32 +0000

    my internet died dakfgjkflgj

---
+++ lotide/findKey.js
@@ -0,0 +1,33 @@
+const findKey = function(obj, callback) {
+
+  for (let index in obj) {
+    if (callback(obj[index])) {
+      return index;
+    }
+  }
+  return undefined
+};
+
+// test case
+let results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// TEST CODE
+assertEqual(results1, "noma");
