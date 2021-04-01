451b722a41b7ba36c6de4566b20d2244435beed6
donnyli95 <sz3li@uwaterloo.ca>
2021-04-01 01:47:20 +0000

    kind of understanding objects now

--- lotide/eqObjects.js
+++ lotide/eqObjects.js
@@ -4,7 +4,6 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   } 
-  
 
   for (const key in object1) {
     if (Array.isArray(object1[key])) {
@@ -12,13 +11,10 @@ const eqObjects = function(object1, object2) {
         return false;
       } 
     } else {
-      if (object1[key] === object2[key]) {
-        continue;
-      } else {
+      if (object1[key] !== object2[key]) {
         return false;
-      }
+      } 
     }
-
   }
 
   return true;
@@ -47,12 +43,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// const ab = { a: "1", b: "2"};
-// const ba = { b: "2", a: "1"};
-// assertEqual(eqObjects(ab, ba), true);
+const ab = { a: "1", b: "2"};
+const ba = { b: "2", a: "1"};
+assertEqual(eqObjects(ab, ba), true);
 
-// const abc = {a: "1", b: "2", c: "3"};
-// assertEqual(eqObjects(ab, abc), false);
+const abc = {a: "1", b: "2", c: "3"};
+assertEqual(eqObjects(ab, abc), false);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1"};
@@ -60,3 +56,6 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+const cd3 = { c: "2", d: ["2", 3] };
+assertEqual(eqObjects(cd, cd3), false);
