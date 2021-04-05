721d01de29049d6e840dabfb180407e1f619b0e4
donnyli95 <sz3li@uwaterloo.ca>
2021-04-05 20:43:52 +0000

    adding mocha and chai testing

--- lotide/test/assertArraysEqualTest.js
+++ lotide/test/assertArraysEqualTest.js
@@ -3,3 +3,4 @@ const assertArraysEqual = require('../assertArraysEqual');
 // TEST ASSERTIONS
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,13 +1,28 @@
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
 const tail = require('../tail');
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 // Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#tail", () => {
+
+  it("returns 2 for result.length", () => {
+    assert.strictEqual(result.length, 2)
+  });
+
+  it("returns 'Lighthouse' for result[0]", () => {
+    assert.strictEqual(result[0], 'Lighthouse')
+  });
+
+  it("returns 'Labs' for result[1]", () => {
+    assert.strictEqual(result[1], 'Labs')
+  });
+});
