1500fcd41220eca066f2f2a46c07185fc29bd816
donnyli95 <sz3li@uwaterloo.ca>
2021-04-05 20:06:46 +0000

    refactoring more exercises

---
+++ lotide/test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST ASSERTIONS
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
