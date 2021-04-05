0bd99b1a640f0ee1a30bf0f3f00eed59e56edfb2
donnyli95 <sz3li@uwaterloo.ca>
2021-04-05 19:51:31 +0000

    refactoring files

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -11,20 +11,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-
-// TEST CASES
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
--- lotide/head.js
+++ lotide/head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   if (array.length > 0) {
     return array[0];
@@ -15,7 +7,5 @@ const head = function(array) {
 
 };
 
-// TEST CASES
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse"); assertEqual(head([]), "Lighthouse");
+module.exports = head;
+
